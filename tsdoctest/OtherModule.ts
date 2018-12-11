d18eb5a1750edda0e35c6f85bdabb8f59c844cde
dinoreborn <dino.reborn@gmail.com>
2018-12-11 10:42:01 +0530

    initial commit

---
+++ tsdoctest/OtherModule.ts
@@ -0,0 +1,23 @@
+﻿/**
+ * This is a documentation for the OtherModule module. It depends on the MyModule module
+ * and contains the [[OtherClass]] class.
+ */
+
+ /** */
+import * as myMd from './MyModule';
+
+/**
+ * This class is inherited from the [[DerivedClass]] class.
+ */
+export class OtherClass extends myMd.DerivedClass {
+    /**
+     * This is an alternative to the [[DerivedClass.theMethod]] method and
+     * the [[mainProp]] property.
+     * @param value A [[DerivedClass]] instance to {@link DerivedClass} process.
+     * @returns The resulting [[MyClass]] instance.
+     */
+    theOtherMethod(value: myMd.DerivedClass): myMd.MyClass {
+        return value;
+    }
+}
+
